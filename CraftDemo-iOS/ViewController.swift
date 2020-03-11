0518e7278e41b86e317f428bd2f5fd8a4102e1c5
Siddharth Suneel <siddharth.suneel@paytm.com>
2020-03-11 20:12:09 +0530

    Initial project setup done + UI skeleton created.

---
+++ CraftDemo-iOS/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  CraftDemo-iOS
+//
+//  Created by Siddharth Suneel on 11/03/20.
+//  Copyright © 2020 Siddharth Suneel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tblView: UITableView! {
+        didSet {
+            tblView.tableFooterView = UIView()
+            tblView.separatorStyle = .none
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 75.0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
